992460dc28a50e21416fb61ae3cf8e70eaae9b85
lifeibiren <lifeibiren@qq.com>
2018-03-22 02:55:01 +0800

    add timeout in udp_server

--- udp_server.hs
+++ udp_server.hs
@@ -1,9 +1,13 @@
 import Data.Bits
 import Network.Socket
 import Network.BSD
-import Data.List
+--import Data.List
+import Data.Map.Strict
+import Data.Time.Clock
 
-type HandlerFunc = Socket -> SockAddr -> String -> IO ()
+type HandlerFunc = Socket -> SockAddr -> String -> UserMap -> IO ()
+type User = SockAddr
+type UserMap = Map User UTCTime
 
 server :: String              -- ^ Port number or name; 514 is default
        -> HandlerFunc         -- ^ Function to handle incoming messages
@@ -23,24 +27,39 @@ server port handlerfunc = withSocketsDo $
        bind sock (addrAddress serveraddr)
 
        -- Loop forever processing incoming data.  Ctrl-C to abort.
-       procMessages sock
-    where procMessages sock =
+       procMessages sock empty
+    where procMessages sock userMap =
               do -- Receive one UDP packet, maximum length 1024 bytes,
                  -- and save its content into msg and its source
                  -- IP and port into addr
                  (msg, _, addr) <- recvFrom sock 1024
+                 time <- getCurrentTime
+                 newUserMap <- foldrWithKey removeDead (return empty) $ insert addr time userMap
                  -- Handle it
-                 handlerfunc sock addr msg
+                 handlerfunc sock addr msg newUserMap
                  -- And process more messages
-                 procMessages sock
+                 procMessages sock newUserMap
+              
+removeDead :: SockAddr -> UTCTime -> IO UserMap -> IO UserMap
+removeDead key val resultIO = do
+    now <- getCurrentTime
+    result <- resultIO
+    let past = diffUTCTime now val
+    if past > 60 -- 60 seconds timeout
+        then return result
+        else return $ insert key val result
+
 
 -- A simple handler that prints incoming packets
 plainHandler :: HandlerFunc
-plainHandler sock addr msg = do
+plainHandler sock addr msg userMap = do
     let output = "From " ++ show addr ++ ": " ++ msg
+    let ret = foldrWithKey concatMsg "" userMap 
     putStrLn output
-    sendTo sock output addr
+    putStrLn ret
+    sendTo sock ret addr
     return ()
+    where concatMsg key _ result = show key ++ "\n" ++ result
 
 main :: IO ()
 main = server "6666" plainHandler
